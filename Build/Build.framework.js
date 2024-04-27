f3cf5ac7fdb3210b5292d26dae35cbc9eba836ad
SHIKINAMl <107552771+SHIKINAMl@users.noreply.github.com>
2024-04-27 17:03:02 +0900

    no message

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -1994,13 +1994,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3674112: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3674173: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3674237: function() {return Module.webglContextAttributes.powerPreference;},  
- 3674295: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3674350: function($0) {performance.now = function() { return $0; };},  
- 3674398: function($0) {performance.now = function() { return $0; };},  
- 3674446: function() {performance.now = Module['emscripten_get_now_backup'];}
+  3674096: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3674157: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3674221: function() {return Module.webglContextAttributes.powerPreference;},  
+ 3674279: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3674334: function($0) {performance.now = function() { return $0; };},  
+ 3674382: function($0) {performance.now = function() { return $0; };},  
+ 3674430: function() {performance.now = Module['emscripten_get_now_backup'];}
 };
 
 
